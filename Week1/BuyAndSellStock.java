754cf4a1c53ff4f71e223b3780a3adeb316d8876
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-13 15:15:51 +0530

    added buyandsellstock

---
+++ Week1/BuyAndSellStock.java
@@ -0,0 +1,19 @@
+package Week1;
+
+public class BuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int cost = prices[i] - mini;
+            maxProfit = Math.max(maxProfit, cost);
+            mini = Math.min(mini, prices[i]);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String args[]) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(prices));
+    }
+}
