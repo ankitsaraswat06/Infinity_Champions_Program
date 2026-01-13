685228d7421bf4963aa4b88378b3419e2013c99e
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-01-13 15:18:08 +0530

    changes in buyandSell

--- Week1/BuyAndSellStock.java
+++ Week1/BuyAndSellStock.java
@@ -4,7 +4,7 @@ public class BuyAndSellStock {
     public static int maxProfit(int[] prices) {
         int mini = prices[0];
         int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++) {
+        for (int i = 1; i < prices.length; i++) {
             int cost = prices[i] - mini;
             maxProfit = Math.max(maxProfit, cost);
             mini = Math.min(mini, prices[i]);
